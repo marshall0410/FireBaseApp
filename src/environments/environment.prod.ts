fdba44a05b69583ec3cee59c937cfc7de37c2c45
Marshall Jones <vondz1991@gmail.com>
2018-02-27 00:29:17 -0500

    First Commit (Testing Firebase)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCDBUmlduKHES8yL-Y3t68X4vdlI1vmcl8',
+    authDomain: 'fbapp-d9391.firebaseapp.com',
+    databaseURL: 'https://fbapp-d9391.firebaseio.com',
+    projectId: 'fbapp-d9391',
+    storageBucket: 'fbapp-d9391.appspot.com',
+    messagingSenderId: '453808145995'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,14 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  firebase: {
+      apiKey: 'AIzaSyCDBUmlduKHES8yL-Y3t68X4vdlI1vmcl8',
+      authDomain: 'fbapp-d9391.firebaseapp.com',
+      databaseURL: 'https://fbapp-d9391.firebaseio.com',
+      projectId: 'fbapp-d9391',
+      storageBucket: 'fbapp-d9391.appspot.com',
+      messagingSenderId: '453808145995'
+    }
+  };
+
